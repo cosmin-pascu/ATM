f98f3ce82ac37dbcdd5c4471491e3f7379e7afcd
cosminiscool <c.pascu99@gmail.com>
2021-12-27 19:02:59 +0200

    Update return type of supply endpoint

--- src/main/java/ATM/demo/controller/BanknoteController.java
+++ src/main/java/ATM/demo/controller/BanknoteController.java
@@ -5,10 +5,14 @@ import ATM.demo.domain.dto.BanknoteResponseDto;
 import ATM.demo.service.BanknoteService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -18,7 +22,7 @@ public class BanknoteController {
     private final BanknoteService banknoteService;
 
     @PostMapping("/supply")
-    public @ResponseBody Banknote supplyAtm(@RequestBody BanknoteResponseDto banknoteResponseDto) {
-        return banknoteService.addBanknote(banknoteResponseDto);
+    public ResponseEntity<Banknote> supplyAtm(@RequestBody BanknoteResponseDto banknoteResponseDto) {
+        return ResponseEntity.of(Optional.of(banknoteService.addBanknote(banknoteResponseDto)));
     }
 }
