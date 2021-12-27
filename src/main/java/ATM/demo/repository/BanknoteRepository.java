9d3b9cc7addaf3f7fdfbc31a6111ee7d33db9bdb
cosminiscool <c.pascu99@gmail.com>
2021-12-27 18:23:25 +0200

    Added search for banknotes by currency and type

--- src/main/java/ATM/demo/repository/BanknoteRepository.java
+++ src/main/java/ATM/demo/repository/BanknoteRepository.java
@@ -1,7 +1,13 @@
 package ATM.demo.repository;
 
 import ATM.demo.domain.Banknote;
+import ATM.demo.domain.enumeration.BanknoteType;
+import ATM.demo.domain.enumeration.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BanknoteRepository extends JpaRepository<Banknote, Long> {
+
+    Optional<Banknote> findByCurrencyAndType(Currency currency, BanknoteType type);
 }
