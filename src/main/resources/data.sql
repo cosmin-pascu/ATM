a70b52e06f4f534635be75a85396c10e3701438d
cosminiscool <c.pascu99@gmail.com>
2021-12-28 17:50:33 +0200

    Added a User in the script for the database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,6 @@ INSERT INTO banknote (currency, type, quantity) VALUES ('RON', 'FIVE', 100);
 INSERT INTO banknote (currency, type, quantity) VALUES ('RON', 'TEN', 100);
 INSERT INTO banknote (currency, type, quantity) VALUES ('RON', 'FIFTY', 50);
 INSERT INTO banknote (currency, type, quantity) VALUES ('RON', 'ONE_HUNDRED', 50);
+
+INSERT INTO user (first_name, last_name, username, password, email, user_type, funds)
+    VALUES ('Cosmin', 'Demo', 'my_username', 'parola', 'eu@mail.com', 'ADMIN', 0);
