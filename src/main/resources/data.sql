87c93d419a45cce22318ea485cf57da5d3974d8b
cosminiscool <c.pascu99@gmail.com>
2021-12-28 15:10:31 +0200

    Create a script to initially populate the database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO banknote (currency, type, quantity) VALUES ('RON', 'ONE', 100);
+INSERT INTO banknote (currency, type, quantity) VALUES ('RON', 'FIVE', 100);
+INSERT INTO banknote (currency, type, quantity) VALUES ('RON', 'TEN', 100);
+INSERT INTO banknote (currency, type, quantity) VALUES ('RON', 'FIFTY', 50);
+INSERT INTO banknote (currency, type, quantity) VALUES ('RON', 'ONE_HUNDRED', 50);
